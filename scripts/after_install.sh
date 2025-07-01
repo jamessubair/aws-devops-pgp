e98da26be5f3e4262d5187a7054f27a584eca996
jamessubair <jamessubair30@gmail.com>
2025-07-01 20:42:20 +0100

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+echo "Running after_install.sh..."
+
+cd /home/ubuntu/zendrix-app
+
+# Install dependencies
+apt-get update -y
+apt-get install -y python3-pip git
+
+pip3 install -r requirements.txt
 
