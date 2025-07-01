a64a96d622d5459ea3cee8f63bbef766d3f03eaa
jamessubair <jamessubair30@gmail.com>
2025-07-01 20:42:38 +0100

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+echo "Running before_install.sh..."
+
+# Stop old app if running
+pkill -f app.py || true
+
+# Clean old code
+rm -rf /home/ubuntu/zendrix-app
+mkdir -p /home/ubuntu/zendrix-app
 
